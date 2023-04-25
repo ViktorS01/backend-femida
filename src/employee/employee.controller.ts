d88f962b2b88552216318a2f4424b231ff3dcdfd
Viktor Sychev <viktor.sychev@softline.com>
2023-04-25 21:36:45 +0400

    add AuthGuard

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -8,31 +8,42 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { EmployeeService } from './employee.service';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('employee')
 export class EmployeeController {
   constructor(private employeeService: EmployeeService) {}
 
+  @UseGuards(AuthGuard)
   @Get()
   getAll() {
     return this.employeeService.findAll();
   }
+
+  @UseGuards(AuthGuard)
   @Get(':id')
   getOne(@Param('id') id: number) {
     return this.employeeService.findOne(id);
   }
+
+  @UseGuards(AuthGuard)
   @Post()
   @HttpCode(HttpStatus.CREATED)
   create(@Body() employee: CreateEmployeeDto) {
     this.employeeService.create(employee);
   }
+
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: number) {
     this.employeeService.remove(id);
   }
+
+  @UseGuards(AuthGuard)
   @Put(':id')
   update(@Body() employeeDto: CreateEmployeeDto, @Param('id') id: number) {
     this.employeeService.update(id, employeeDto);
